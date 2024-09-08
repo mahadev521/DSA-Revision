97a8f7eae6dc2e212cccb0905801582179432426
Mahadev <srimanthmahadev22@gmail.com>
2024-09-08 22:33:33 +0530

    solved problem to find primes less than n

---
+++ Leetcode solved problems/SolvePrimes/solution.py
@@ -0,0 +1,11 @@
+def countPrimes(self, n: int) -> int:
+    if n<3 : return 0
+    lst = [1]*n
+    lst[0]=0
+    lst[1]=0
+    m=2
+    while m*m<n:
+        if lst[m]==1:
+            lst[m * m: n: m] = [0] *(1 + (n - m * m - 1) // m)
+        m+=1 if m==2 else 2
+    return sum(lst)
